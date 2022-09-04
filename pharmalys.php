51975d992e1bc4a018aabe6a21e443f1341b3882
Reza Khan <rezakhan995@gmail.com>
2022-09-04 10:46:20 +0600

    plugin header updated

--- pharmalys.php
+++ pharmalys.php
@@ -1,17 +1,29 @@
 <?php
 
 /**
- * Plugin Name:       My Basics Plugin
- * Plugin URI:        https://example.com/plugins/the-basics/
+ * Plugin Name:       Pharmalys Essential
+ * Plugin URI:        https://reza-khan.com/pharmalys/
  * Description:       Handle the basics with this plugin.
- * Version:           1.10.3
+ * Version:           1.0.0
  * Requires at least: 5.2
- * Requires PHP:      7.2
- * Author:            John Smith
- * Author URI:        https://author.example.com/
+ * Requires PHP:      7.3
+ * Author:            Reza Khan
+ * Author URI:        https://reza-khan.com/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Update URI:        https://example.com/my-plugin/
- * Text Domain:       my-basics-plugin
+ * Text Domain:       pharmalys-essential
  * Domain Path:       /languages
- */
+ 
+ * Pharmalys Essential is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+    
+ * Pharmalys Essential is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+    
+ * You should have received a copy of the GNU General Public License
+ * along with Pharmalys Essential. If not, see <http://www.gnu.org/licenses/>.
+*/
