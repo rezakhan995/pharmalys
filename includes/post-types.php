7c844b3c751c958a07a18b1b699bb25f293fd357
Reza Khan <rezakhan995@gmail.com>
2022-09-07 18:08:52 +0600

    more files added to avoid collision

--- includes/post-types.php
+++ includes/post-types.php
@@ -6,3 +6,17 @@
  * @subpackage Functions
  * @since 1.0.0
  */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Register Post Types
+ * 
+ * @since 1.0.0
+ * @return void
+ */
+function pe_setup_post_types(){
+    $archives = true;
+    $slug = '';
+}
+add_action('init', 'pe_setup_post_types', 1);
