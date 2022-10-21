31b93dbdd6419ce4798706506e9328a611abc553
Reza Khan <rezakhan995@gmail.com>
2022-10-21 16:10:53 +0600

    validated with phpcs

--- includes/class-pharmalys-essential.php
+++ includes/class-pharmalys-essential.php
@@ -2,220 +2,220 @@
 
 defined( 'ABSPATH' ) || exit;
 
-if ( !class_exists( 'Pharmalys_Essential' ) ) {
+if ( ! class_exists( 'Pharmalys_Essential' ) ) {
 
-    /**
-     * Pharmalys_Essential class.
-     *
-     * @since 1.0.0
-     */
-    final class Pharmalys_Essential {
+	/**
+	 * Pharmalys_Essential class.
+	 *
+	 * @since 1.0.0
+	 */
+	final class Pharmalys_Essential {
 
-        /**
-         * @var Pharmalys_Essential The Actual Pharmalys_Essential instance
-         * @since 1.0.0
-         */
-        private static $instance;
+		/**
+		 * @var Pharmalys_Essential The Actual Pharmalys_Essential instance
+		 * @since 1.0.0
+		 */
+		private static $instance;
 
-        /**
-         * Main File
-         */
-        private $file = '';
+		/**
+		 * Main File
+		 */
+		private $file = '';
 
-        /**
-         * Throw Error While Trying To Clone Object
-         *
-         * @since 1.0.0
-         * @return void
-         */
-        public function __clone() {
-            _doing_it_wrong( __FUNCTION__, __( 'Cloning is forbidden.', 'pharmalys-essential' ), '1.0.0' );
-        }
+		/**
+		 * Throw Error While Trying To Clone Object
+		 *
+		 * @since 1.0.0
+		 * @return void
+		 */
+		public function __clone() {
+			_doing_it_wrong( __FUNCTION__, __( 'Cloning is forbidden.', 'pharmalys-essential' ), '1.0.0' );
+		}
 
-        /**
-         * Disabling Un-serialization Of This Class
-         */
-        public function __wakeup() {
-            _doing_it_wrong( __FUNCTION__, __( 'Unserializing instances of this class is forbidden.', 'pharmalys-essential' ), '1.0.0' );
-        }
+		/**
+		 * Disabling Un-serialization Of This Class
+		 */
+		public function __wakeup() {
+			_doing_it_wrong( __FUNCTION__, __( 'Unserializing instances of this class is forbidden.', 'pharmalys-essential' ), '1.0.0' );
+		}
 
-        /**
-         * The actual Pharmalys_Essential instance
-         *
-         * @since 1.0.0
-         * @param string $file
-         * @return void
-         */
-        public static function instantiate( $file = '' ) {
+		/**
+		 * The actual Pharmalys_Essential instance
+		 *
+		 * @since 1.0.0
+		 * @param string $file
+		 * @return void
+		 */
+		public static function instantiate( $file = '' ) {
 
-            // Return if already instantiated
-            if ( self::instantiated() ) {
-                return self::$instance;
-            }
+			// Return if already instantiated
+			if ( self::instantiated() ) {
+				return self::$instance;
+			}
 
-            self::prepare_instance( $file );
+			self::prepare_instance( $file );
 
-            self::$instance->initialize_constants();
-            self::$instance->define_tables();
-            self::$instance->include_files();
-            self::$instance->initialize_hooks();
+			self::$instance->initialize_constants();
+			self::$instance->define_tables();
+			self::$instance->include_files();
+			self::$instance->initialize_hooks();
 
-            return self::$instance;
+			return self::$instance;
 
-        }
+		}
 
-        /**
-         * Return If The Main Class has Already Been Instantiated Or Not
-         *
-         * @since 1.0.0
-         * @return boolean
-         */
-        private static function instantiated() {
-            if ( ( null !== self::$instance ) && ( self::$instance instanceof Pharmalys_Essential ) ) {
-                return true;
-            }
+		/**
+		 * Return If The Main Class has Already Been Instantiated Or Not
+		 *
+		 * @since 1.0.0
+		 * @return boolean
+		 */
+		private static function instantiated() {
+			if ( ( null !== self::$instance ) && ( self::$instance instanceof Pharmalys_Essential ) ) {
+				return true;
+			}
 
-            return false;
-        }
+			return false;
+		}
 
-        /**
-         * Prepare Singleton Instance
-         *
-         * @since 1.0.0
-         * @param string $file
-         * @return void
-         */
-        private static function prepare_instance( $file = '' ) {
-            self::$instance             = new self();
-            self::$instance->file       = $file;
-            self::$instance->version    = Pharmalys_Essential_Prepare::get_version();
-        }
+		/**
+		 * Prepare Singleton Instance
+		 *
+		 * @since 1.0.0
+		 * @param string $file
+		 * @return void
+		 */
+		private static function prepare_instance( $file = '' ) {
+			self::$instance          = new self();
+			self::$instance->file    = $file;
+			self::$instance->version = Pharmalys_Essential_Prepare::get_version();
+		}
 
-        /**
-         * Assets Directory URL
-         *
-         * @since 1.0.0
-         * @return void
-         */
-        public function get_assets_url(){
-            return trailingslashit( PE_PLUGIN_URL . 'assets' );
-        }
+		/**
+		 * Assets Directory URL
+		 *
+		 * @since 1.0.0
+		 * @return void
+		 */
+		public function get_assets_url() {
+			return trailingslashit( PE_PLUGIN_URL . 'assets' );
+		}
 
-        /**
-         * Assets Directory Path
-         *
-         * @since 1.0.0
-         * @return void
-         */
-        public function get_assets_dir(){
-            return trailingslashit( PE_PLUGIN_DIR . 'assets' );
-        }
+		/**
+		 * Assets Directory Path
+		 *
+		 * @since 1.0.0
+		 * @return void
+		 */
+		public function get_assets_dir() {
+			return trailingslashit( PE_PLUGIN_DIR . 'assets' );
+		}
 
-        /**
-         * Plugin Directory URL
-         *
-         * @return void
-         */
-        public function get_plugin_url(){
-            return trailingslashit( plugin_dir_url( PE_PLUGIN_FILE ) );
-        }
+		/**
+		 * Plugin Directory URL
+		 *
+		 * @return void
+		 */
+		public function get_plugin_url() {
+			return trailingslashit( plugin_dir_url( PE_PLUGIN_FILE ) );
+		}
 
-        /**
-         * Plugin Directory Path
-         *
-         * @return void
-         */
-        public function get_plugin_dir(){
-            return Pharmalys_Essential_Prepare::get_plugin_dir();
-        }
+		/**
+		 * Plugin Directory Path
+		 *
+		 * @return void
+		 */
+		public function get_plugin_dir() {
+			return Pharmalys_Essential_Prepare::get_plugin_dir();
+		}
 
-        /**
-         * Plugin Basename
-         *
-         * @return void
-         */
-        public function get_plugin_basename(){
-            return plugin_basename( PE_PLUGIN_FILE );
-        }
+		/**
+		 * Plugin Basename
+		 *
+		 * @return void
+		 */
+		public function get_plugin_basename() {
+			return plugin_basename( PE_PLUGIN_FILE );
+		}
 
-        /**
-         * Setup Plugin Constants
-         *
-         * @since 1.0.0
-         * @return void
-         */
-        private function initialize_constants() {
+		/**
+		 * Setup Plugin Constants
+		 *
+		 * @since 1.0.0
+		 * @return void
+		 */
+		private function initialize_constants() {
 
-            // Plugin Version
-            self::$instance->define( 'PE_VERSION', Pharmalys_Essential_Prepare::get_version() );
+			// Plugin Version
+			self::$instance->define( 'PE_VERSION', Pharmalys_Essential_Prepare::get_version() );
 
-            //  Plugin Main File
-            self::$instance->define( 'PE_PLUGIN_FILE', $this->file );
+			// Plugin Main File
+			self::$instance->define( 'PE_PLUGIN_FILE', $this->file );
 
-            // Plugin File Basename
-            self::$instance->define( 'PE_PLUGIN_BASE', $this->get_plugin_basename() );
+			// Plugin File Basename
+			self::$instance->define( 'PE_PLUGIN_BASE', $this->get_plugin_basename() );
 
-            // Plugin Main Directory Path
-            self::$instance->define( 'PE_PLUGIN_DIR', $this->get_plugin_dir() );
+			// Plugin Main Directory Path
+			self::$instance->define( 'PE_PLUGIN_DIR', $this->get_plugin_dir() );
 
-            // Plugin Main Directory URL
-            self::$instance->define( 'PE_PLUGIN_URL', $this->get_plugin_url() );
+			// Plugin Main Directory URL
+			self::$instance->define( 'PE_PLUGIN_URL', $this->get_plugin_url() );
 
-            // Plugin Assets Directory URL
-            self::$instance->define( 'PE_ASSETS_URL', $this->get_assets_url() );
+			// Plugin Assets Directory URL
+			self::$instance->define( 'PE_ASSETS_URL', $this->get_assets_url() );
 
-            // Plugin Assets Directory Path
-            self::$instance->define( 'PE_ASSETS_DIR', $this->get_assets_dir() );
+			// Plugin Assets Directory Path
+			self::$instance->define( 'PE_ASSETS_DIR', $this->get_assets_dir() );
 
-        }
-        /**
-         * Define constant if not already set.
-         *
-         * @since 1.0.0
-         * @param string      $name  Constant name.
-         * @param string|bool $value Constant value.
-         */
-        private function define( $name, $value ) {
-            if ( ! defined( $name ) ) {
-                define( $name, $value );
-            }
-        }
+		}
+		/**
+		 * Define constant if not already set.
+		 *
+		 * @since 1.0.0
+		 * @param string      $name  Constant name.
+		 * @param string|bool $value Constant value.
+		 */
+		private function define( $name, $value ) {
+			if ( ! defined( $name ) ) {
+				define( $name, $value );
+			}
+		}
 
 
-        /**
-         * Define DB Tables Required For This Plugin
-         *
-         * @since 1.0.0
-         * @return void
-         */
-        private function define_tables(){
-            # To Be Implemented
-        }
+		/**
+		 * Define DB Tables Required For This Plugin
+		 *
+		 * @since 1.0.0
+		 * @return void
+		 */
+		private function define_tables() {
+			// To Be Implemented
+		}
 
-        /**
-         * Include All Required Files
-         *
-         * @since 1.0.0
-         * @return void
-         */
-        private function include_files() {
-            require_once PE_PLUGIN_DIR . 'includes/post-types.php';
-            require_once PE_PLUGIN_DIR . 'includes/widgets/manifest.php';
-            require_once PE_PLUGIN_DIR . 'includes/template-functions.php';
-            require_once PE_PLUGIN_DIR . 'includes/shortcodes.php';
-            require_once PE_PLUGIN_DIR . 'includes/install.php';
-        }
+		/**
+		 * Include All Required Files
+		 *
+		 * @since 1.0.0
+		 * @return void
+		 */
+		private function include_files() {
+			require_once PE_PLUGIN_DIR . 'includes/post-types.php';
+			require_once PE_PLUGIN_DIR . 'includes/widgets/manifest.php';
+			require_once PE_PLUGIN_DIR . 'includes/template-functions.php';
+			require_once PE_PLUGIN_DIR . 'includes/shortcodes.php';
+			require_once PE_PLUGIN_DIR . 'includes/install.php';
+		}
 
-        /**
-         * Initialize All Hooks
-         * 
-         * @since 1.0.0
-         * @return void
-         */
-        private function initialize_hooks(){
-            # To be Implemented
-        }
-    }
+		/**
+		 * Initialize All Hooks
+		 *
+		 * @since 1.0.0
+		 * @return void
+		 */
+		private function initialize_hooks() {
+			// To be Implemented
+		}
+	}
 
 }
 
@@ -227,5 +227,5 @@ if ( !class_exists( 'Pharmalys_Essential' ) ) {
  * @return Pharmalys_Essential
  */
 function PE() {
-    return Pharmalys_Essential::instantiate();
+	return Pharmalys_Essential::instantiate();
 }
--- includes/post-types.php
+++ includes/post-types.php
@@ -2,6 +2,7 @@
 
 /**
  * Functions Related To Post Types
+ *
  * @package PE
  * @subpackage Functions
  * @since 1.0.0
@@ -16,17 +17,20 @@ defined( 'ABSPATH' ) || exit;
  * @return void
  */
 function pe_add_main_menu_page() {
-    add_action( 'admin_menu', function () {
-        add_menu_page(
-            __( 'Pharmalys', 'pharmalys-essential' ),
-            __( 'Pharmalys', 'pharmalys-essential' ),
-            'read',
-            'pharmalys-essential',
-            '',
-            'dashicons-store',
-            10
-        );
-    } );
+	add_action(
+		'admin_menu',
+		function () {
+			add_menu_page(
+				__( 'Pharmalys', 'pharmalys-essential' ),
+				__( 'Pharmalys', 'pharmalys-essential' ),
+				'read',
+				'pharmalys-essential',
+				'',
+				'dashicons-store',
+				10
+			);
+		}
+	);
 }
 
 add_action( 'init', 'pe_add_main_menu_page', 0 );
@@ -38,112 +42,118 @@ add_action( 'init', 'pe_add_main_menu_page', 0 );
  * @return void
  */
 function pe_setup_post_types() {
-    $archives = true;
-    $slug     = 'pe-products';
-    $rewrite  = [
-        'slug'       => $slug,
-        'with_front' => true,
-        'pages'      => true,
-        'feeds'      => false,
-    ];
+	$archives = true;
+	$slug     = 'pe-products';
+	$rewrite  = array(
+		'slug'       => $slug,
+		'with_front' => true,
+		'pages'      => true,
+		'feeds'      => false,
+	);
 
-    /** Register Product Post Type */
-    $product_labels = apply_filters( 'pe_product_labels', [
-        'name'                  => _x( '%2$s', 'product post type name', 'pharmalys-essential' ),
-        'singular_name'         => _x( '%1$s', 'singular product post type name', 'pharmalys-essential' ),
-        'add_new'               => __( 'Add New', 'pharmalys-essential' ),
-        'add_new_item'          => __( 'Add New %1$s', 'pharmalys-essential' ),
-        'edit_item'             => __( 'Edit %1$s', 'pharmalys-essential' ),
-        'new_item'              => __( 'New %1$s', 'pharmalys-essential' ),
-        'all_items'             => __( '%2$s', 'pharmalys-essential' ),
-        'view_item'             => __( 'View %1$s', 'pharmalys-essential' ),
-        'search_items'          => __( 'Search %2$s', 'pharmalys-essential' ),
-        'not_found'             => __( 'No %2$s found', 'pharmalys-essential' ),
-        'not_found_in_trash'    => __( 'No %2$s found in Trash', 'pharmalys-essential' ),
-        'parent_item_colon'     => '',
-        'menu_name'             => _x( '%2$s', 'product post type menu name', 'pharmalys-essential' ),
-        'featured_image'        => __( '%1$s Image', 'pharmalys-essential' ),
-        'set_featured_image'    => __( 'Set %1$s Image', 'pharmalys-essential' ),
-        'remove_featured_image' => __( 'Remove %1$s Image', 'pharmalys-essential' ),
-        'use_featured_image'    => __( 'Use as %1$s Image', 'pharmalys-essential' ),
-        'attributes'            => __( '%1$s Attributes', 'pharmalys-essential' ),
-        'filter_items_list'     => __( 'Filter %2$s list', 'pharmalys-essential' ),
-        'items_list_navigation' => __( '%2$s list navigation', 'pharmalys-essential' ),
-        'items_list'            => __( '%2$s list', 'pharmalys-essential' ),
-    ] );
+	/** Register Product Post Type */
+	$product_labels = apply_filters(
+		'pe_product_labels',
+		array(
+			'name'                  => _x( '%2$s', 'product post type name', 'pharmalys-essential' ),
+			'singular_name'         => _x( '%1$s', 'singular product post type name', 'pharmalys-essential' ),
+			'add_new'               => __( 'Add New', 'pharmalys-essential' ),
+			'add_new_item'          => __( 'Add New %1$s', 'pharmalys-essential' ),
+			'edit_item'             => __( 'Edit %1$s', 'pharmalys-essential' ),
+			'new_item'              => __( 'New %1$s', 'pharmalys-essential' ),
+			'all_items'             => __( '%2$s', 'pharmalys-essential' ),
+			'view_item'             => __( 'View %1$s', 'pharmalys-essential' ),
+			'search_items'          => __( 'Search %2$s', 'pharmalys-essential' ),
+			'not_found'             => __( 'No %2$s found', 'pharmalys-essential' ),
+			'not_found_in_trash'    => __( 'No %2$s found in Trash', 'pharmalys-essential' ),
+			'parent_item_colon'     => '',
+			'menu_name'             => _x( '%2$s', 'product post type menu name', 'pharmalys-essential' ),
+			'featured_image'        => __( '%1$s Image', 'pharmalys-essential' ),
+			'set_featured_image'    => __( 'Set %1$s Image', 'pharmalys-essential' ),
+			'remove_featured_image' => __( 'Remove %1$s Image', 'pharmalys-essential' ),
+			'use_featured_image'    => __( 'Use as %1$s Image', 'pharmalys-essential' ),
+			'attributes'            => __( '%1$s Attributes', 'pharmalys-essential' ),
+			'filter_items_list'     => __( 'Filter %2$s list', 'pharmalys-essential' ),
+			'items_list_navigation' => __( '%2$s list navigation', 'pharmalys-essential' ),
+			'items_list'            => __( '%2$s list', 'pharmalys-essential' ),
+		)
+	);
 
-    foreach ( $product_labels as $key => $value ) {
-        $product_labels[$key] = sprintf( $value, pe_get_label_singular( 'product' ), pe_get_label_plural( 'product' ) );
-    }
+	foreach ( $product_labels as $key => $value ) {
+		$product_labels[ $key ] = sprintf( $value, pe_get_label_singular( 'product' ), pe_get_label_plural( 'product' ) );
+	}
 
-    $product_args = [
-        'labels'             => $product_labels,
-        'public'             => true,
-        'publicly_queryable' => true,
-        'show_ui'            => true,
-        'show_in_menu'       => true,
-        'query_var'          => true,
-        'menu_icon'          => 'dashicons-archive',
-        'rewrite'            => $rewrite,
-        'capability_type'    => 'post',
-        'map_meta_cap'       => true,
-        'has_archive'        => $archives,
-        'hierarchical'       => false,
-        'show_in_rest'       => true,
-        'rest_base'          => 'pe-products',
-        'show_in_menu'       => 'pharmalys-essential',
-        'supports'           => apply_filters( 'pe_product_supports', ['title', 'editor', 'thumbnail', 'revisions', 'author'] ),
-    ];
-    register_post_type( 'pe-product', apply_filters( 'pe_product_post_type_args', $product_args ) );
+	$product_args = array(
+		'labels'             => $product_labels,
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'menu_icon'          => 'dashicons-archive',
+		'rewrite'            => $rewrite,
+		'capability_type'    => 'post',
+		'map_meta_cap'       => true,
+		'has_archive'        => $archives,
+		'hierarchical'       => false,
+		'show_in_rest'       => true,
+		'rest_base'          => 'pe-products',
+		'show_in_menu'       => 'pharmalys-essential',
+		'supports'           => apply_filters( 'pe_product_supports', array( 'title', 'editor', 'thumbnail', 'revisions', 'author' ) ),
+	);
+	register_post_type( 'pe-product', apply_filters( 'pe_product_post_type_args', $product_args ) );
 
-    /** Register Product Post Type */
-    $story_labels = apply_filters( 'pe_story_labels', [
-        'name'                  => _x( '%2$s', 'story post type name', 'pharmalys-essential' ),
-        'singular_name'         => _x( '%1$s', 'singular story post type name', 'pharmalys-essential' ),
-        'add_new'               => __( 'Add New', 'pharmalys-essential' ),
-        'add_new_item'          => __( 'Add New %1$s', 'pharmalys-essential' ),
-        'edit_item'             => __( 'Edit %1$s', 'pharmalys-essential' ),
-        'new_item'              => __( 'New %1$s', 'pharmalys-essential' ),
-        'all_items'             => __( '%2$s', 'pharmalys-essential' ),
-        'view_item'             => __( 'View %1$s', 'pharmalys-essential' ),
-        'search_items'          => __( 'Search %2$s', 'pharmalys-essential' ),
-        'not_found'             => __( 'No %2$s found', 'pharmalys-essential' ),
-        'not_found_in_trash'    => __( 'No %2$s found in Trash', 'pharmalys-essential' ),
-        'parent_item_colon'     => '',
-        'menu_name'             => _x( '%2$s', 'story post type menu name', 'pharmalys-essential' ),
-        'featured_image'        => __( '%1$s Image', 'pharmalys-essential' ),
-        'set_featured_image'    => __( 'Set %1$s Image', 'pharmalys-essential' ),
-        'remove_featured_image' => __( 'Remove %1$s Image', 'pharmalys-essential' ),
-        'use_featured_image'    => __( 'Use as %1$s Image', 'pharmalys-essential' ),
-        'attributes'            => __( '%1$s Attributes', 'pharmalys-essential' ),
-        'filter_items_list'     => __( 'Filter %2$s list', 'pharmalys-essential' ),
-        'items_list_navigation' => __( '%2$s list navigation', 'pharmalys-essential' ),
-        'items_list'            => __( '%2$s list', 'pharmalys-essential' ),
-    ] );
+	/** Register Product Post Type */
+	$story_labels = apply_filters(
+		'pe_story_labels',
+		array(
+			'name'                  => _x( '%2$s', 'story post type name', 'pharmalys-essential' ),
+			'singular_name'         => _x( '%1$s', 'singular story post type name', 'pharmalys-essential' ),
+			'add_new'               => __( 'Add New', 'pharmalys-essential' ),
+			'add_new_item'          => __( 'Add New %1$s', 'pharmalys-essential' ),
+			'edit_item'             => __( 'Edit %1$s', 'pharmalys-essential' ),
+			'new_item'              => __( 'New %1$s', 'pharmalys-essential' ),
+			'all_items'             => __( '%2$s', 'pharmalys-essential' ),
+			'view_item'             => __( 'View %1$s', 'pharmalys-essential' ),
+			'search_items'          => __( 'Search %2$s', 'pharmalys-essential' ),
+			'not_found'             => __( 'No %2$s found', 'pharmalys-essential' ),
+			'not_found_in_trash'    => __( 'No %2$s found in Trash', 'pharmalys-essential' ),
+			'parent_item_colon'     => '',
+			'menu_name'             => _x( '%2$s', 'story post type menu name', 'pharmalys-essential' ),
+			'featured_image'        => __( '%1$s Image', 'pharmalys-essential' ),
+			'set_featured_image'    => __( 'Set %1$s Image', 'pharmalys-essential' ),
+			'remove_featured_image' => __( 'Remove %1$s Image', 'pharmalys-essential' ),
+			'use_featured_image'    => __( 'Use as %1$s Image', 'pharmalys-essential' ),
+			'attributes'            => __( '%1$s Attributes', 'pharmalys-essential' ),
+			'filter_items_list'     => __( 'Filter %2$s list', 'pharmalys-essential' ),
+			'items_list_navigation' => __( '%2$s list navigation', 'pharmalys-essential' ),
+			'items_list'            => __( '%2$s list', 'pharmalys-essential' ),
+		)
+	);
 
-    foreach ( $story_labels as $key => $value ) {
-        $story_labels[$key] = sprintf( $value, pe_get_label_singular( 'story' ), pe_get_label_plural( 'story' ) );
-    }
+	foreach ( $story_labels as $key => $value ) {
+		$story_labels[ $key ] = sprintf( $value, pe_get_label_singular( 'story' ), pe_get_label_plural( 'story' ) );
+	}
 
-    $story_args = [
-        'labels'             => apply_filters( 'pe_story_labels', $story_labels ),
-        'public'             => true,
-        'publicly_queryable' => true,
-        'show_ui'            => true,
-        'show_in_menu'       => true,
-        'query_var'          => true,
-        'menu_icon'          => 'dashicons-edit-page',
-        'rewrite'            => $rewrite,
-        'capability_type'    => 'post',
-        'map_meta_cap'       => true,
-        'has_archive'        => $archives,
-        'hierarchical'       => false,
-        'show_in_rest'       => true,
-        'rest_base'          => 'pe-stories',
-        'show_in_menu'       => 'pharmalys-essential',
-        'supports'           => apply_filters( 'pe_story_supports', ['title', 'editor', 'thumbnail', 'revisions', 'author'] ),
-    ];
-    register_post_type( 'pe-story', apply_filters( 'pe_story_post_type_args', $story_args ) );
+	$story_args = array(
+		'labels'             => apply_filters( 'pe_story_labels', $story_labels ),
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'menu_icon'          => 'dashicons-edit-page',
+		'rewrite'            => $rewrite,
+		'capability_type'    => 'post',
+		'map_meta_cap'       => true,
+		'has_archive'        => $archives,
+		'hierarchical'       => false,
+		'show_in_rest'       => true,
+		'rest_base'          => 'pe-stories',
+		'show_in_menu'       => 'pharmalys-essential',
+		'supports'           => apply_filters( 'pe_story_supports', array( 'title', 'editor', 'thumbnail', 'revisions', 'author' ) ),
+	);
+	register_post_type( 'pe-story', apply_filters( 'pe_story_post_type_args', $story_args ) );
 }
 
 add_action( 'init', 'pe_setup_post_types', 1 );
@@ -156,50 +166,50 @@ add_action( 'init', 'pe_setup_post_types', 1 );
  */
 function pe_get_default_product_labels( $term ) {
 
-    $labels = [];
+	$labels = array();
 
-    switch ( $term ) {
-    case 'product':
-        $labels = [
-            'singular' => __( 'Product', 'pharmalys-essential' ),
-            'plural'   => __( 'Products', 'pharmalys-essential' ),
-        ];
-        break;
-    case 'story':
-        $labels = [
-            'singular' => __( 'Story', 'pharmalys-essential' ),
-            'plural'   => __( 'Stories', 'pharmalys-essential' ),
-        ];
-        break;
-    }
+	switch ( $term ) {
+		case 'product':
+			$labels = array(
+				'singular' => __( 'Product', 'pharmalys-essential' ),
+				'plural'   => __( 'Products', 'pharmalys-essential' ),
+			);
+			break;
+		case 'story':
+			$labels = array(
+				'singular' => __( 'Story', 'pharmalys-essential' ),
+				'plural'   => __( 'Stories', 'pharmalys-essential' ),
+			);
+			break;
+	}
 
-    return $labels;
+	return $labels;
 }
 
 /**
  * Singular Label For CPT
  *
  * @since 1.0.0
- * @param [type] $term
+ * @param [type]  $term
  * @param boolean $lowercase
  * @return void
  */
 function pe_get_label_singular( $term, $lowercase = false ) {
-    $defaults = pe_get_default_product_labels( $term );
-    return $lowercase ? strtolower( $defaults['singular'] ) : $defaults['singular'];
+	$defaults = pe_get_default_product_labels( $term );
+	return $lowercase ? strtolower( $defaults['singular'] ) : $defaults['singular'];
 }
 
 /**
  * Plural Label For CPT
  *
  * @since 1.0.0
- * @param [type] $term
+ * @param [type]  $term
  * @param boolean $lowercase
  * @return void
  */
 function pe_get_label_plural( $term, $lowercase = false ) {
-    $defaults = pe_get_default_product_labels( $term );
-    return $lowercase ? strtolower( $defaults['plural'] ) : $defaults['plural'];
+	$defaults = pe_get_default_product_labels( $term );
+	return $lowercase ? strtolower( $defaults['plural'] ) : $defaults['plural'];
 }
 
 /**
